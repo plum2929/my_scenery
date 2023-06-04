3b4110b69e17a291e142e3cd8e2a75ada33f0f4b
plum2929 <105092969+plum2929@users.noreply.github.com>
2023-06-04 16:36:31 +0900

    Add: 写真投稿機能を追加

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,7 +3,24 @@ class PhotosController < ApplicationController
 
   def show; end
 
-  def new; end
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = current_user.photos.build(photo_params)
+    if @photo.save
+      redirect_to photos_path
+    else
+      render :new
+    end
+  end
 
   def edit; end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:image, :image_cache)
+  end
 end
