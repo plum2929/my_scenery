15774f7814112d92930a41cf52b8d3f40f5a1ae1
plum2929 <105092969+plum2929@users.noreply.github.com>
2023-06-09 19:33:10 +0900

    Add :フラッシュメッセージに動きを追加

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import ImageController from "./image_controller"
 application.register("image", ImageController)
+
+import Notification from "stimulus-notification"
+application.register("notification", Notification)
