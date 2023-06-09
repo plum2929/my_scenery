0a7b858ceb75ccb96949d45fa37933b4fdd4476c
plum2929 <105092969+plum2929@users.noreply.github.com>
2023-06-09 19:34:27 +0900

    Fix: ログイン時のリダイレクトを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
-  add_flash_types :success, :warning
+  add_flash_types :info, :success, :warning
 
   private
 
   def not_authenticated
     redirect_to login_path, warning: t('defaults.require_login')
+  end
+
+  def no_login_required
+    redirect_to photos_path if logged_in?
   end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   skip_before_action :require_login
 
-  def top; end
+  def top
+    redirect_to photos_path if logged_in?
+  end
 end
