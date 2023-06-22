089f62f425d004f59cf6df63896c8147b6e5124b
plum2929 <105092969+plum2929@users.noreply.github.com>
2023-06-22 18:37:58 +0900

    Fix: ヘッダー表示の不具合を修正

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -8,28 +8,35 @@ export default class extends Controller {
     const header = this.headerTarget
     const headerHeight = header.offsetHeight
     const container = document.getElementsByClassName('container')[0]
+    let margin_top = ''
     let scrollPoint = 0
     let lastPoint = 0
+
+    if (headerHeight === 48) {
+      margin_top = 'mt-12'
+    }else if (headerHeight === 56) {
+      margin_top = 'mt-14'
+    }
 
     window.addEventListener("scroll", () => {
       scrollPoint = window.scrollY
 
       if (scrollPoint < headerHeight) {
         header.classList.remove('fixed-hide','fixed', 'top-0')
-        container.classList.remove(`mt-[${headerHeight}px]`)
+        container.classList.remove(margin_top)
         if (scrollPoint < lastPoint) {
           header.classList.add('fixed', 'top-0')
           header.classList.remove('fixed-hide')
-          container.classList.add(`mt-[${headerHeight}px]`)
+          container.classList.add(margin_top)
         }
       }else{
         if(scrollPoint > lastPoint){
           header.classList.add('fixed-hide', 'fixed', 'top-0')
-          container.classList.add(`mt-[${headerHeight}px]`)
+          container.classList.add(margin_top)
         }else{
           header.classList.add('fixed', 'top-0')
           header.classList.remove('fixed-hide')
-          container.classList.add(`mt-[${headerHeight}px]`)
+          container.classList.add(margin_top)
         }
       }
       lastPoint = scrollPoint
