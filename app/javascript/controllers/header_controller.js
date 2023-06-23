f9dffbaf8b7fc17d43dd4d078604533f84f3c1bd
plum2929 <105092969+plum2929@users.noreply.github.com>
2023-06-23 18:18:24 +0900

    Fix: 不要なスペースを削除

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -12,19 +12,19 @@ export default class extends Controller {
     let scrollPoint = 0
     let lastPoint = 0
 
-    if (headerHeight === 48) {
+    if(headerHeight === 48) {
       margin_top = 'mt-12'
-    }else if (headerHeight === 56) {
+    }else if(headerHeight === 56) {
       margin_top = 'mt-14'
     }
 
     window.addEventListener("scroll", () => {
       scrollPoint = window.scrollY
 
-      if (scrollPoint < headerHeight) {
+      if(scrollPoint < headerHeight) {
         header.classList.remove('fixed-hide','fixed', 'top-0')
         container.classList.remove(margin_top)
-        if (scrollPoint < lastPoint) {
+        if(scrollPoint < lastPoint) {
           header.classList.add('fixed', 'top-0')
           header.classList.remove('fixed-hide')
           container.classList.add(margin_top)
