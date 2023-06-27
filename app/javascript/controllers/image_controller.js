117ca5085d5f5e3ceebd4648f3d74891a6b68267
plum2929 <105092969+plum2929@users.noreply.github.com>
2023-06-27 16:34:48 +0900

    Add: 画像未選択時は投稿ボタンを無効化

--- app/javascript/controllers/image_controller.js
+++ app/javascript/controllers/image_controller.js
@@ -1,8 +1,21 @@
 import { Controller } from "@hotwired/stimulus"
+import { addFieldInvalidToClassList, changeDisabledStatusSubmitBtn, checkBlank } from './form_validation'
 
 // Connects to data-controller="image"
 export default class extends Controller {
-  static targets = ['select', 'preview']
+  static targets = ['select', 'preview', 'submit']
+
+  connect() {
+    const select = this.selectTarget
+    const submitBtn = this.submitTarget
+
+    addFieldInvalidToClassList([select])
+
+    select.addEventListener('change', () => {
+      checkBlank(select)
+      changeDisabledStatusSubmitBtn(submitBtn)
+    })
+  }
 
   imagePreview() {
     const file = this.selectTarget.files[0]
